52e6ee40bca9b220a59efd38631114fe4d42b1d1
AVKostin <alexander.kostin.ua@gmail.com>
2022-03-05 09:13:08 +0200

    Create fetch_api.js

---
+++ src/js/fetch_api.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api/';
+
+export default class PicturesApiService {
+	constructor() {
+		this.searchQuery = '';
+		this.page = 1;
+	}
+
+	async fetchPictures() {
+		try {
+			const searchParams = new URLSearchParams({
+				key: '25823737-b417134f62dbe2cba56a97bcd',
+				q: this.searchQuery,
+				image_type: 'photo',
+				per_page: 40,
+				orientation: 'horizontal',
+				page: this.page,
+			});
+
+			const url = `${BASE_URL}?${searchParams}`;
+
+			const response = await axios.get(url);
+
+			return response;
+		} catch (error) {
+			return error;
+		}
+	}
+
+	incrementPage() {
+		this.page += 1;
+	}
+
+	resetPage() {
+		this.page = 1;
+	}
+
+	get query() {
+		return this.searchQuery;
+	}
+
+	set query(newQuery) {
+		this.searchQuery = newQuery;
+	}
+}
