df4bfb7faaf4ec565cf8f8a216f1d85b34b027cb
AVKostin <alexander.kostin.ua@gmail.com>
2022-03-31 10:47:55 +0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './sass/main.scss';
+import '../node_modules/@fortawesome/fontawesome-free/js/solid.min';
+import '../node_modules/@fortawesome/fontawesome-free/js/fontawesome.min';
+import './js/fetch_api';
+import './js/rendered_cards';
+import './js/to_top';
