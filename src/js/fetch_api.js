c4df26fd5bd6e60e66b652c52328a80f98f3de11
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-11-09 20:21:34 +0200

    refactoring

--- src/js/fetch_api.js
+++ src/js/fetch_api.js
@@ -1,47 +1,44 @@
-import axios from 'axios';
+import axios from "axios";
 
-const BASE_URL = 'https://pixabay.com/api/';
+const BASE_URL = "https://pixabay.com/api/";
 
 export default class PicturesApiService {
-	constructor() {
-		this.searchQuery = '';
-		this.page = 1;
-	}
+    constructor() {
+        this.searchQuery = "";
+        this.page = 1;
+    }
 
-	async fetchPictures() {
-		try {
-			const searchParams = new URLSearchParams({
-				key: '25823737-b417134f62dbe2cba56a97bcd',
-				q: this.searchQuery,
-				image_type: 'photo',
-				per_page: 40,
-				orientation: 'horizontal',
-				page: this.page,
-			});
+    async fetchPictures() {
+        try {
+            const searchParams = new URLSearchParams({
+                key: "25823737-b417134f62dbe2cba56a97bcd",
+                q: this.searchQuery,
+                image_type: "photo",
+                per_page: 40,
+                orientation: "horizontal",
+                page: this.page,
+            });
+            const url = `${BASE_URL}?${searchParams}`;
+            const response = await axios.get(url);
+            return response;
+        } catch (error) {
+            return error;
+        }
+    }
 
-			const url = `${BASE_URL}?${searchParams}`;
+    incrementPage() {
+        this.page += 1;
+    }
 
-			const response = await axios.get(url);
+    resetPage() {
+        this.page = 1;
+    }
 
-			return response;
-		} catch (error) {
-			return error;
-		}
-	}
+    get query() {
+        return this.searchQuery;
+    }
 
-	incrementPage() {
-		this.page += 1;
-	}
-
-	resetPage() {
-		this.page = 1;
-	}
-
-	get query() {
-		return this.searchQuery;
-	}
-
-	set query(newQuery) {
-		this.searchQuery = newQuery;
-	}
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
 }
